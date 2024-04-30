73805bcc7c6081238d2b384c649557ad21bde586
barrantes4539 <kevinbarrantes02@gmail.com>
2024-04-30 16:57:39 -0600

    Auth system

--- store/views.py
+++ store/views.py
@@ -1,10 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Product
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
-# Create your views here.
+#Store
 def home(request):
     products = Product.objects.all()
     return render(request, 'home.html', {'products':products})
+
 def about(request):
     return render(request, 'about.html', {})
+
+#Authentication
+def login_user(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request,user)
+            messages.success(request, ("Has iniciado Sesión"))
+            return redirect('home')
+        else:
+            messages.success(request, ("Ha habido un error, intente de nuevo"))
+            return redirect('login')
+    else:
+        return render(request, 'login.html', {})
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, ("Has cerrado sesión"))
+    return redirect('home')
 
